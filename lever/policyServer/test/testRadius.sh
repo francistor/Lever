df82004d3783cd067c3501b40fc20394bba493a7
francisco.cardosogil@gmail.com <francisco.cardosogil@gmail.com>
2015-03-29 18:12:32 +0200

    29/03 samsung

--- lever/policyServer/test/testRadius.sh
+++ lever/policyServer/test/testRadius.sh
@@ -2,7 +2,7 @@
 #  --------------------------------------------------------------
 #  TECO RADIUS TEST
 #
-#  Parameter is number of packets to send
+#  Usage: testRadius.sh auth|acct|all <number-of-packets>
 #  --------------------------------------------------------------
 
 AAABASEDIR=/home/francisco/AAA
@@ -31,38 +31,51 @@ NAS_Port=\"1\"
 Acct_Session_Id=1-1
 Framed_IP_Address=10.0.0.1
 
-count=${1:-1}
+command=${1:-all}
+count=${2:-1}
 overlap=1
 
 #  Delete Garbage
 rm out/* > /dev/null 2>&1
 
-#  Access request -------------------------------------------------------------
-echo
-echo Access request
-echo
+if [ ${command} = "auth" ] || [ ${command} = "all" ];
+then
 
-#  Compose the packet
-echo User-Name=${User_Name} > ${REQUESTFILE}
-echo Acct-Session-Id=${Acct_Session_Id} >> ${REQUESTFILE}
-echo NAS-IP-Address=${NAS_IP_Address} >> ${REQUESTFILE}
-echo NAS-Port=${NAS_Port} >> ${REQUESTFILE}
+    #  Access request -------------------------------------------------------------
+    echo
+    echo Access request
+    echo
 
-#  Send the packet
-${RADIUSAUTH} -count ${count} -overlap ${overlap} -debug debug -secret ${Secret} -code Access-Request -request @${REQUESTFILE} -outputAttributesFile ${TESTFILES}\out\access-request.txt
+    #  Compose the packet
+    echo User-Name=${User_Name} > ${REQUESTFILE}
+    echo Acct-Session-Id=${Acct_Session_Id} >> ${REQUESTFILE}
+    echo NAS-IP-Address=${NAS_IP_Address} >> ${REQUESTFILE}
+    echo NAS-Port=${NAS_Port} >> ${REQUESTFILE}
+
+    #  Send the packet
+    ${RADIUSAUTH} -count ${count} -overlap ${overlap} -debug debug -secret ${Secret} -code Access-Request -request @${REQUESTFILE} -outputAttributesFile ${TESTFILES}\out\access-request.txt
+
+fi
+
+if [ ${command} = "acct"  ] || [ ${command} = "all" ];
+then
+
+    #  Account Start -------------------------------------------------------------
+    echo
+    echo Account start
+    echo
+
+    #  Compose the packet
+    echo User-Name=${User_Name}>${REQUESTFILE}
+    echo Acct-Session-Id=${Acct_Session_Id}>>${REQUESTFILE}
+    echo NAS-IP-Address=${NAS_IP_Address}>>${REQUESTFILE}
+    echo NAS-Port=${NAS_Port}>>${REQUESTFILE}
+    echo Acct-Status-Type="Start">>${REQUESTFILE}
+
+    #  Send the packet
+    ${RADIUSACCT} -count ${count} -overlap ${overlap} -debug debug -secret ${Secret} -code Accounting-Request -request @${REQUESTFILE} -outputAttributesFile ${TESTFILES}\out\account-start.txt
+
+fi
+exit
 
 
-#  Account Start
-echo
-echo Account start
-echo
-
-#  Compose the packet
-echo User-Name=${User_Name}>${REQUESTFILE}
-echo Acct-Session-Id=${Acct_Session_Id}>>${REQUESTFILE}
-echo NAS-IP-Address=${NAS_IP_Address}>>${REQUESTFILE}
-echo NAS-Port=${NAS_Port}>>${REQUESTFILE}
-echo Acct-Status-Type="Start">>${REQUESTFILE}
-
-#  Send the packet
-${RADIUSACCT} -count ${count} -overlap ${overlap} -debug debug -secret ${Secret} -code Accounting-Request -request @${REQUESTFILE} -outputAttributesFile ${TESTFILES}\out\account-start.txt
